3d424889957800eeb87635249ed2f1155a57a1df
Muhammad Azeem <mailmeatazeem@gmail.com>
2024-06-16 13:34:05 +0500

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// Code
+
+class Solution {
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+
+        long long int n = x, rev = 0;
+
+        while (n > 0)
+        {
+            rev = (rev * 10) + (n % 10);
+            n = n / 10;
+        }
+
+        if(rev == x)
+            return true;
+        else
+            return false;
+    }
+};
